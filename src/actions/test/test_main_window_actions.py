e30ab8378df50bac7d35b3b03dc55e2426190aa0
Martin Manns <mmanns@gmx.net>
2011-06-21 23:52:08 +0200

    main_window actions test skeleton set up

--- src/actions/test/test_main_window_actions.py
+++ src/actions/test/test_main_window_actions.py
@@ -12,16 +12,19 @@ from gui._main_window import MainWindow
 from gui._grid import Grid
 import lib.vartypes as vartypes
 from lib._interfaces import PysInterface
-from model._data_array import DataArray
+from model.model import DataArray
 
 import actions._main_window_actions as main_window_actions
 
-class TestCsvGenerator(object):
+
+class TestCsvInterface(object):
     def setup_method(self, method):
-        pass
+        self.test_filename = "actions/test/test.csv"
+        self.test_filename2 = "actions/test/test_one_col.csv"
+        self.test_filename3 = "actions/test/test_write.csv"
         
         
-    def get_csv_gen(self, filename, digest_types=None):
+    def _get_csv_gen(self, filename, digest_types=None):
         test_file = open(filename)
         dialect = csv.Sniffer().sniff(test_file.read(1024))
         test_file.close()
@@ -31,13 +34,13 @@ class TestCsvGenerator(object):
             
         has_header = False
 
-        return main_window_actions.CsvGenerator( \
+        return main_window_actions.CsvInterface( \
             filename, dialect, digest_types, has_header)
 
     def test_get_csv_cells_gen(self):
         """Tests generator from csv content"""
-        test_filename = "test.csv"
-        csv_gen = self.get_csv_gen(test_filename)
+        
+        csv_gen = self._get_csv_gen(self.test_filename)
         
         column = xrange(100)
         
@@ -50,14 +53,11 @@ class TestCsvGenerator(object):
     def test_iter(self):
         """Tests csv generator"""
         
-        test_filename = "test.csv"
-        csv_gen = self.get_csv_gen(test_filename)
+        csv_gen = self._get_csv_gen(self.test_filename)
         
         assert [list(col) for col in csv_gen] == [['1', '2'], ['3', '4']]
         
-
-        test_filename = "test_one_col.csv"
-        csv_gen = self.get_csv_gen(test_filename, digest_types = [type("")])
+        csv_gen = self._get_csv_gen(self.test_filename2, digest_types = [type("")])
         
         
         for i, col in enumerate(csv_gen):
@@ -66,7 +66,78 @@ class TestCsvGenerator(object):
                 assert list_col == ["'" + str(i + 1) + "'", "''"]
             else:
                 assert list_col == ["''", "''"]
+    
+    def test_write(self):
+        """Tests writing csv file"""
         
+        csv_gen = self._get_csv_gen(self.test_filename)
+        csv_gen.path = self.test_filename3
         
+        csv_gen.write(xrange(100) for _ in xrange(100))
+        
+        infile = open(self.test_filename3)
+        content = infile.read()
+        assert content[:10] == "0\t1\t2\t3\t4\t"
+        infile.close()
+
+
+class TestTxtGenerator(object):
+    """Tests generating txt files"""
+    
+    def test_iter(self):
+        pass
+
+
 class TestExchangeActions(object):
-    pass
+    def test_import_csv(self):
+        pass
+        
+    def test_import_txt(self):
+        pass
+    
+    def test_import_file(self):
+        pass
+    
+    def test_export_csv(self):
+        pass
+    
+    def test_export_file(self):
+        pass
+
+class TestPrintActions(object):
+    def test_print_preview(self):
+        pass
+        
+    def test_printout(self):
+        pass
+    
+class TestClipboardActions(object):
+    def test_cut(self):
+        pass
+        
+    def test_copy(self):
+        pass
+        
+    def test_copy_result(self):
+        pass
+        
+    def test_paste(self):
+        pass
+        
+class TestMacroActions(object):
+    def test_replace_macros(self):
+        pass
+
+    def test_execute_macros(self):
+        pass
+        
+    def test_open_macros(self):
+        pass
+        
+    def test_save_macros(self):
+        pass
+
+class TestHelpActions(object):
+    def test_launch_help(self):
+        pass
+    
